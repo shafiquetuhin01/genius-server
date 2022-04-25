1f6561cde66c80343a934a44af31dda4f8c1c318
BiasDesk1 <shafiquetuhin01>
2022-04-25 16:06:17 +0600

    server updating

--- index.js
+++ index.js
@@ -34,6 +34,21 @@ async function run() {
       const service = await serviceCollection.findOne(query);
       res.send(service);
     });
+
+    //post
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    //delete
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
